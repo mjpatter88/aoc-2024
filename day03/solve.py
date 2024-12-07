8bcf8f770d582bfb0f1cb1f8e45c5bbf96ed4894
Michael Patterson <mjpatter88@gmail.com>
2024-12-07 14:08:04 -0600

    Solve day3 part 2.

--- day03/solve.py
+++ day03/solve.py
@@ -4,13 +4,13 @@ def solve():
   with open("input.txt") as in_file:
     code = in_file.read().strip()
 
-  muls = re.findall("mul\([0-9]+,[0-9]+\)", code)
+  muls = re.findall(r"mul\([0-9]+,[0-9]+\)", code)
   # print(muls)
 
   total = 0
   for mul in muls:
     # Capture the first and second numbers
-    p = re.compile("mul\(([0-9]+),([0-9]+)\)")
+    p = re.compile(r"mul\(([0-9]+),([0-9]+)\)")
     factors = p.match(mul)
     assert(factors)
     if factors:
@@ -19,6 +19,35 @@ def solve():
 
   print(f"Solution to part 1: {total}")
 
+  second_total = 0
+
+  dos_and_donts = [match for match in re.finditer(r"(don't|do)", code)]
+  toggles = [(d.start(), d.groups()[0]) for d in dos_and_donts]
+  # print(toggles)
+
+  muls = [match for match in re.finditer(r"mul\(([0-9]+),([0-9]+)\)", code)]
+  for mul in muls:
+    position = mul.start()
+    first, second = mul.groups()
+    # print(position)
+    # print(f"{first},{second}")
+
+    if is_active(toggles, position):
+      second_total += int(first) * int(second)
+
+  print(f"Solution to part 2: {second_total}")
+
+def is_active(toggles: list[tuple[int, str]], position: int) -> bool:
+  # Walk backwards through the list of toggles
+  for toggle in reversed(toggles):
+    if toggle[0] < position:
+      if toggle[1] == "don't":
+        return False
+      else:
+        return True
+
+  # If no toggle applies, then it is active
+  return True
 
 
 
